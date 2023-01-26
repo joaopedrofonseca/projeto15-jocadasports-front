25f2c470e53578e67e5ac82dd219dd86de27ab59
joaopedrofonseca <joaopedrofonsec4@gmail.com>
2023-01-26 14:03:08 -0300

    feat: login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cadastro from "./pages/Cadastro";
 import Login from "./pages/Login";
 
 
 function App() {
+  const [token, setToken] = useState(undefined)
+
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login/>}/>
+      <Route path="/" element={<Login setToken={setToken}/>}/>
       <Route path="/cadastro" element={<Cadastro/>}/>
     </Routes>
     </BrowserRouter>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,44 @@
-export default function Login(){
-    return(
-        <h1>Em construção</h1>
+import axios from "axios"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { AuthScreen, Warning } from "../components/Auth.Screen"
+
+
+export default function Login({setToken}) {
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [erro, setErro] = useState(undefined)
+    const navigate = useNavigate()
+
+    function login(e){
+        e.preventDefault()
+        const body = {email, senha}
+        axios.post(`${process.env.REACT_APP_API_URL}/login`, body)
+        .then(res => {
+            //navigate("/produtos")
+            setToken(res.data)
+            console.log(res.data)})
+        .catch(err => setErro(err.response.request.status))
+    }
+
+    return (
+        <AuthScreen>
+            <h1>Jocada Sports</h1>
+            <form onSubmit={login}>
+                <input placeholder="email" type="email" value={email} onChange={e => setEmail(e.target.value)} required></input>
+                <input placeholder="senha" type="password" value={senha} onChange={e => setSenha(e.target.value)} required></input>
+                <button type="submit">Entrar</button>
+            </form>
+            {erro &&
+                <Warning>
+                    <ion-icon name="alert-circle-outline"></ion-icon><h3>Email/senha inválidos</h3>
+                </Warning>
+            }
+            <Link to="/cadastro" style={{ textDecoration: 'none' }}>
+                <h2>Primeira vez? Cadastre-se!</h2>
+            </Link>
+        </AuthScreen>
+
+
     )
 }
